f84a5cc99f6b54c49f196b3d0b690b103638b45a
Antoine Lefeuvre <lefeuvre@novius.com>
2013-01-17 18:21:29 +0100

    Gilles est un relou - Can I say that in a commit message

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -7,7 +7,7 @@
  *             http://www.gnu.org/licenses/agpl-3.0.html
  * @link http://www.novius-os.org
  */
- 
+
 return array(
     'name'    => 'Comments (required for both Blog or News stories)',
     'version' => '0.2',
