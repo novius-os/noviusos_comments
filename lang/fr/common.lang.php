a8c266496a2858e99e75b6a3c41a6759d5114128
esperat <esperat@novius.fr>
2013-01-23 11:46:04 +0100

    "Added french translations for all applications"

---
+++ lang/fr/common.lang.php
@@ -0,0 +1,36 @@
+<?php
+
+// Generated on 23/01/2013 11:25:53
+
+// 9 out of 9 messages are translated (100%).
+// 34 out of 34 words are translated (100%).
+
+return array(
+    #: views/front/form.view.php:10
+    'Leave a comment' => 'Laisser un commentaire',
+
+    #: views/front/form.view.php:19
+    'You failed the captcha test. Please try again.' => 'Vous n’avez pas passé le test du captcha. Merci de ré-essayer.',
+
+    #: views/front/form.view.php:25
+    'Your comment has been successfully added.' => 'Votre commentaire a bien été ajouté.',
+
+    #: views/front/form.view.php:34
+    'Name:' => 'Nom&nbsp;:',
+
+    #: views/front/form.view.php:38
+    'Email address (never sold, shared nor spammed):' => 'Email (jamais vendu, partagé ou spammé)&nbsp;:',
+
+    #: views/front/form.view.php:42
+    'Your comment:' => 'Votre commentaire&nbsp;:',
+
+    #: views/front/form.view.php:61
+    'Send' => 'Envoyer',
+
+    #: views/front/item.view.php:3
+    'Comment by {{author}}' => 'Commentaire par {{author}}',
+
+    #: views/front/item.view.php:4
+    '%d/%m/%Y at %H:%M' => '%d/%m/%Y à %H:%M',
+
+);
--- lang/fr/unused.lang.php
+++ lang/fr/unused.lang.php
@@ -6,7 +6,7 @@
 // 8 out of 8 words are translated (100%).
 
 return array(
-    #. Overwritten
+    #: metadata
     'Comments (required for both Blog and News stories)' => 'Commentaires (nécessaire pour Blog ou Actualités)',
 
 );
