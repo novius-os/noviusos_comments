1d237457a7ec99d806219acaa0dd7a389b308927
Sébastien Drouyer <drouyer@novius.com>
2013-05-14 17:04:32 +0200

    removed unused comments

--- config/common/comment.config.php
+++ config/common/comment.config.php
@@ -19,7 +19,7 @@ return array(
                 if ($item->is_new()) {
                     return null;
                 }
-                return \Date::create_from_string($item->comm_created_at, 'mysql')->format('%m/%d/%Y %H:%M:%S'); //%m/%d/%Y %H:%i:%s
+                return \Date::create_from_string($item->comm_created_at, 'mysql')->format('%m/%d/%Y %H:%M:%S');
             },
         ),
         'preview_url' => array(
@@ -51,80 +51,4 @@ return array(
             ),
         ),
     )
-    /*
-    'i18n' => array(
-        // Crud
-        'notification item added' => __('Done! The item has been added.'),
-        'notification item saved' => __('OK, all changes are saved.'),
-        'notification item deleted' => __('The item has been deleted.'),
-
-        // General errors
-        'notification item does not exist anymore' => __('This item doesn’t exist any more. It has been deleted.'),
-        'notification item not found' => __('We cannot find this item.'),
-        'deleted popup title' => __('Bye bye'),
-        'deleted popup close' => __('Close tab'),
-
-        // see novius-os/framework/config/i18n_common.config.php
-    ),
-    */
-    /*
-    'actions' => array(
-        'delete' => array(
-            'action' => array( // ce qu'on envoi à nosAction
-                'action' => 'confirmationDialog',
-                    'dialog' => array(
-                    'contentUrl' => '{{controller_base_url}}delete/{{_id}}',
-                    'title' => 'Delete',
-                ),
-            ),
-            'label' => __('Delete'),
-            'primary' => true,
-            'icon' => 'trash',
-            'red' => true,
-            'targets' => array(
-                'grid' => true,
-                'toolbar-edit' => true,
-            ),
-            'disabled' => function($item) {
-                return false;
-            },
-            'visible' => function($params) {
-                return !isset($params['item']) || !$params['item']->is_new();
-            },
-        ),
-    )
-    */
-    /*
-    'actions' => array(
-        'list' => array(
-            'delete' => array(
-                'action' => array( // ce qu'on envoi à nosAction
-                    'action' => 'confirmationDialog',
-                    'dialog' => array(
-                        'contentUrl' => '{{controller_base_url}}delete/{{_id}}',
-                        'title' => 'Delete',
-                    ),
-                ),
-                'label' => __('Delete'),
-                'primary' => true,
-                'icon' => 'trash',
-                'red' => true,
-                'targets' => array(
-                'grid' => true,
-                    'toolbar-edit' => true,
-                ),
-                'disabled' => function($item) {
-                        return false;
-                },
-                'visible' => function($params) {
-                        return !isset($params['item']) || !$params['item']->is_new();
-                },
-            ),
-        ),
-        'order' => array(
-            'delete',
-            // others
-        ),
-    )
-    */
 );
--- config/controller/admin/comment/crud.config.php
+++ config/controller/admin/comment/crud.config.php
@@ -5,7 +5,6 @@ return array(
     'layout' => array(
         'large' => true,
         'save' => 'save',
-        //'title' => 'comm_content',
         'content' => array(
             'test' => array(
                 'view' => 'nos::form/expander',
@@ -86,31 +85,4 @@ return array(
             ),
         ),
     )
-    /* UI texts sample
-    'messages' => array(
-        'successfully added' => __('Item successfully added.'),
-        'successfully saved' => __('Item successfully saved.'),
-        'successfully deleted' => __('Item has successfully been deleted!'),
-        'you are about to delete, confim' => __('You are about to delete item <span style="font-weight: bold;">":title"</span>. Are you sure you want to continue?'),
-        'you are about to delete' => __('You are about to delete item <span style="font-weight: bold;">":title"</span>.'),
-        'exists in multiple context' => __('This item exists in <strong>{count} contexts</strong>.'),
-        'delete in the following contexts' => __('Delete this item in the following contexts:'),
-        'item deleted' => __('This item has been deleted.'),
-        'not found' => __('Item not found'),
-        'error added in context' => __('This item cannot be added {context}.'),
-        'item inexistent in context yet' => __('This item has not been added in {context} yet.'),
-        'add an item in context' => __('Add a new item in {context}'),
-        'delete an item' => __('Delete a item'),
-    ),
-    */
-    /*
-    Tab configuration sample
-    'tab' => array(
-        'iconUrl' => 'static/apps/{{application_name}}/img/16/icon.png',
-        'labels' => array(
-            'insert' => __('Add a item'),
-            'blankSlate' => __('Translate a item'),
-        ),
-    ),
-    */
 );
