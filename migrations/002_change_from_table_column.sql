f01504e1c1b71e55b659ecbc404f2cbe4920ad31
Gilles FELIX <felix@novius.com>
2013-06-04 15:46:01 +0200

    "Remove interclassement utf8_general_ci from migration SQL scripts"

--- migrations/002_change_from_table_column.sql
+++ migrations/002_change_from_table_column.sql
@@ -1,3 +1,3 @@
-ALTER TABLE  `nos_comment` CHANGE  `comm_from_table`  `comm_foreign_model` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE  `nos_comment` CHANGE  `comm_from_table`  `comm_foreign_model` VARCHAR( 255 ) NOT NULL;
 UPDATE `nos_comment` SET comm_foreign_model = 'Nos\\BlogNews\\Blog\\Model_Post' WHERE comm_foreign_model = 'nos_blog_post';
 UPDATE `nos_comment` SET comm_foreign_model = 'Nos\\BlogNews\\News\\Model_Post' WHERE comm_foreign_model = 'nos_news_post';
