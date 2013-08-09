3bb6be585b009f27fd020740c16cc08857c360f1
Sébastien Drouyer <drouyer@novius.com>
2013-08-09 17:07:22 +0200

    moved some functions to behaviour

--- classes/orm/behaviour/commentable.php
+++ classes/orm/behaviour/commentable.php
@@ -17,8 +17,6 @@ class Orm_Behaviour_Commentable extends \Nos\Orm_Behaviour
         \Nos\I18n::current_dictionary('noviusos_comments::common');
     }
 
-    protected $_comments_relation = null;
-
     /**
      * parent_relation
      * children_relation
@@ -28,30 +26,98 @@ class Orm_Behaviour_Commentable extends \Nos\Orm_Behaviour
     public function __construct($class)
     {
         parent::__construct($class);
-        $this->_comments_relation = call_user_func($class . '::relations', $this->_properties['comments_relation']);
 
-        if (false === $this->_comments_relation) {
-            throw new \Exception('Relation "comments" not found by commentable behaviour: '.$this->_class);
+        if (!isset($this->_properties['show_states'])) {
+            $this->_properties['show_states'] = array('published');
+        }
+        if (!is_array($this->_properties['show_states'])) {
+            $this->_properties['show_states'] = array($this->_properties['show_states']);
         }
     }
 
+    /**
+     * Add relations for linked media and wysiwyg shared with other context
+     */
+    public function buildRelations()
+    {
+        $class = $this->_class;
+        $pk = $class::primary_key();
+        $pk = $pk[0];
 
-    protected $nb_comments = null;
+        $class::addRelation('has_many', 'comments', array(
+            'key_from' => $pk,
+            'model_to' => '\Nos\Comments\Model_Comment',
+            'key_to' => 'comm_foreign_id',
+            'cascade_save' => false,
+            'cascade_delete' => true,
+            'conditions' => array(
+                'where' => array(
+                    array('comm_foreign_model', '=', $class),
+                    array('comm_state', 'IN', $this->_properties['show_states']),
+                ),
+                'order_by' => array('comm_created_at' => 'ASC')
+            ),
+        ));
+    }
+
+    public function getProperties()
+    {
+        return $this->_properties;
+    }
+
+
+    protected $nb_comments = array();
     public function count_comments(\Nos\Orm\Model $item)
     {
-        if ($this->nb_comments === null) {
-            $this->nb_comments = \Nos\Comments\Model_Comment::count(
-                array(
-                    'where' => array(
-                        array('comm_foreign_id' => $item->id),
-                        array('comm_foreign_model' => $this->_class),
-                        array('comm_state' => 'published'),
+        if (!isset($this->nb_comments[$item->id])) {
+            $item->setNbComments(
+                \Nos\Comments\Model_Comment::count(
+                    array(
+                        'where' => array(
+                            array('comm_foreign_id' => $item->id),
+                            array('comm_foreign_model' => $this->_class),
+                            array('comm_state', 'IN', $this->_properties['show_states']),
+                        )
                     )
                 )
             );
         }
+        return $this->nb_comments[$item->id];
+    }
 
-        return $this->nb_comments;
+    public function setNbComments(\Nos\Orm\Model $item, $nb)
+    {
+        $this->nb_comments[$item->id] = $nb;
+    }
+
+    public function count_multiple_comments($items)
+    {
+        $class = $this->_class;
+        $ids = array();
+
+        foreach ($items as $post) {
+            $ids[] = $post->id;
+        }
+
+        $comments_count = \Db::select(\Db::expr('COUNT(comm_id) AS count_result'), 'comm_foreign_id')
+            ->from(\Nos\Comments\Model_Comment::table())
+            ->where('comm_foreign_id', 'in', $ids)
+            ->and_where('comm_foreign_model', '=', $class)
+            ->and_where('comm_state', 'IN', $this->_properties['show_states'])
+            ->group_by('comm_foreign_id')
+            ->execute()->as_array();
+
+        $comments_count = \Arr::assoc_to_keyval($comments_count, 'comm_foreign_id', 'count_result');
+
+        foreach ($items as $key => $item) {
+            if (isset($comments_count[$items[$key]->id])) {
+                $items[$key]->setNbComments($comments_count[$items[$key]->id]);
+            } else {
+                $items[$key]->setNbComments(0);
+            }
+        }
+
+        return $items;
     }
 
 }
