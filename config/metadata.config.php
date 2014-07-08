72dfc2f398e50af7a419e1a740c1d524eb92fb3e
Gilles FELIX <felix@novius.com>
2014-07-08 09:31:25 +0200

    "Dubrovka 4.3"

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -10,7 +10,7 @@
 
 return array(
     'name'    => 'Comments',
-    'version' => '4.2 (Dubrovka)',
+    'version' => '4.3 (Dubrovka)',
     'provider' => array(
         'name' => 'NoviusOS',
     ),
